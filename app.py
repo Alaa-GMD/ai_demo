d264d51d00aa0517adb00a79fbb72bd50c6072db
Alaa-GMD <97918353+Alaa-GMD@users.noreply.github.com>
2022-08-18 09:18:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -79,15 +79,13 @@ image_path= 'dog.jpg'
 config= 'yolov3-tiny.cfg'
 yolo_classes = 'yolov3.txt'
 
-def sepia(input_img):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189], 
-        [0.349, 0.686, 0.168], 
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = input_img.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return sepia_img
+# start gradio demo
+with gr.Blocks() as demo:
+    gr.Markdown("Detect objects in a given image using Yolo")
+    with gr.Row():
+        image_input = gr.Image()
+        image_output = gr.Image()
+    image_button = gr.Button("Detect objects")
+    image_button.click(detect_objects, inputs=image_input, outputs=image_output)
 
-demo = gr.Interface(detect_objects, gr.Image(shape=(200, 200)), "image")
 demo.launch()
